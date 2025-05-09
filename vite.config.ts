73e49b2dbc33cf92804f22c1e176bdf49534581a
chereolisa <justincherechukwu@gmail.com>
2025-05-09 15:33:16 +0100

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true  // Important for axios
+    }
+  },
+  optimizeDeps: {
+    include: ['axios']  // Explicitly include axios
+  }
 })
